a3828b4f52c55f5bcff7479cdfcedc5f03f9b24e
Jerry Lokoroi <jerrylokoroi@gmail.com>
2023-04-11 09:49:51 +0300

    LoropioFitnessApp start

---
+++ LoropioFitnessApp/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Please enter you name: ");
+string lionName = Console.ReadLine();
+
+Console.WriteLine("Hello " + lionName + ". " + "Welcome to the Loropio Fitness App.");
