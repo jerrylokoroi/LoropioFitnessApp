698bce3aef52f89171a64d19eb32fb11e5ce2d29
Jerry Lokoroi <jerrylokoroi@gmail.com>
2023-06-02 20:21:17 +0300

    Created SportActivity.cs use it to create two methods that calculate average speed in km/h and m/s then created Dialog.cs for creating a method csalled StartDialog for user selections. Also created ActivityDialog.cs

---
+++ LoropioFitnessApp/LoropioFitnessApp/Program.cs
@@ -0,0 +1,9 @@
+﻿using LoropioFitnessApp;
+
+Console.WriteLine("Please enter you name: ");
+
+string lionName = Console.ReadLine();
+
+Console.WriteLine("Hello " + lionName + ". " + "Welcome to the Loropio Fitness App.");
+
+Dialog.StartDialog();
