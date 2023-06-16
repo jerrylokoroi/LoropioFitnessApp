60083aa37bf07e36dc2d3ea335ebe4b12c0f6cee
Jerry Lokoroi <jerrylokoroi@gmail.com>
2023-06-16 11:36:37 +0300

    create a string array refactor your EnterActivity method

---
+++ LoropioFitnessApp/LoropioFitnessApp/Dialog.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoropioFitnessApp
+{
+    internal class Dialog
+    {
+        public static void StartDialog()
+        {
+            int userSelection;
+            do
+            {
+                Console.WriteLine("Please select an option:");
+                Console.WriteLine("1. Enter new sport activity");
+                Console.WriteLine("2. View all activities");
+                Console.WriteLine("3. Load specific sport activity");
+                Console.WriteLine("4. Quit application");
+
+                userSelection = int.Parse(Console.ReadLine());
+
+                switch (userSelection)
+                {
+                    case 1:
+                        ActivityDialog.EnterActivity();
+                        Console.WriteLine("New sport activity entered successfully!");
+                        // Do something with the new activity, such as saving it or displaying it
+                        break;
+                    case 2:
+                        Console.WriteLine("What type of sports activity do you want to enter ?");
+                        Console.WriteLine("1. Bike SportActivity");
+                        Console.WriteLine("2. Climb SportActivity");
+                        Console.WriteLine("3. Run SportActivity");
+                        Console.WriteLine("4. Swim SportActivity");
+
+                        string activityTypeInput = Console.ReadLine();
+
+                        if(activityTypeInput != null)
+                        {
+                            Console.WriteLine("A new line activity has been entered successfully");
+                        }
+                        else
+                        {
+                            Console.WriteLine("invalid sport activity");
+                        }
+                        break;
+                    case 3:
+                        // Implement load specific sport activity
+                        break;
+                    case 4:
+                        Console.WriteLine("Thank you for using Loropio Fitness App. Goodbye!");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid selection. Please try again.");
+                        break;
+                }
+            } while (userSelection != 4);
+        }
+
+    }
+}
---
+++ LoropioFitnessApp/LoropioFitnessApp/SportActivity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoropioFitnessApp
+{
+    internal class SportActivity
+    {
+
+        public static double CalculateAverageSpeedInKmPerHour(double distance, TimeOnly time)
+        {
+            
+            double totalSeconds = time.Hour * 3600 + time.Minute * 60 + time.Second;
+            double totalHours = totalSeconds / 3600;
+            double averageSpeed = (distance / totalHours); 
+            return Math.Round(averageSpeed, 2);
+        }
+
+        public static double CalculateAverageSpeedMilePerSecond(double distance, TimeOnly time)
+        {
+            double totalSeconds = time.Hour * 3600 + time.Minute * 60 + time.Second;
+            double distanceInMiles = distance / 1.60934; //Converting KM to Miles
+            double averageSpeed = distanceInMiles / totalSeconds;
+            return Math.Round(averageSpeed, 2);
+        }
+    }
+}
